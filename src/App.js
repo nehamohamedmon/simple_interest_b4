1758d6c07fbb93ef6b9517b60c7107c011415690
nehamohamedmon <nnehamohamedmon619@gmail.com>
2023-10-25 13:11:17 +0530

    simple interest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,118 @@
-import logo from './logo.svg';
+import { TextField } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+       const [interest , setInterest] = useState(0)
+       const [principle , setPrinciple] = useState(0)
+       const [rate , setRate] = useState(0)
+       const [year , setYear] = useState(0)
+       const [isprinciple , setIsPrinciple] = useState(true)
+       const [israte , setIsRate] = useState()
+       const [isyear , setIsYear] = useState()
+
+    const getValidate  = (e)=>{
+     const {name , value}=e.target
+     /*console.log(name , value);*/
+     /*console.log(typeof(value));*/ 
+     /*console.log(!!value.match (/^[0-9]+$/))*/ //!!to convert in to boolean
+     if(!!value.match (/^[0-9]*.?[0-9]+$/)){
+      if(name==='principle'){
+        setPrinciple(value)
+      setIsPrinciple(true)}
+      else if(name==='rate'){
+        setRate(value)
+        setIsRate(true)
+      }
+      else{
+        setYear(value)
+        setIsYear(true)
+      }
+     }
+     else{
+       if(name==='principle'){
+        setIsPrinciple(value)
+        setIsPrinciple(false)}
+        else if(name==='rate'){
+          setRate(value)
+          setIsRate(false)
+        }
+        else{
+          setYear(value)
+          setIsYear(false)
+        }
+     }
+    }
+    const handleCalculate = (e)=>{
+      e.preventDefault()
+      if(!principle || !rate || !year){
+        alert('please fill the form')
+      }
+      else{
+       /* alert('submitted')*/
+       setInterest(principle*rate*year/100)
+      }
+    }
+
+    const handleReset = (e)=>{
+      setInterest(0)
+      setPrinciple(0)
+      setRate(0)
+      setYear(0)
+      setIsPrinciple(true)
+      setIsRate(true)
+      setIsYear(true)
+
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'100vh'}} className='d-flex justify-content-center align-items-center w-100 bg-dark'>
+      <div className='bg-light p-5 rounded'>
+         <h1>Simple Interest App</h1>
+         <p>Calculate simple interest Easily</p>
+         <div className='bg-warning d-flex justify-content-center align-items-center w-100 p-3 flex-column rounded mt-4 shadow'>
+            <h1>₹ {''} {interest}</h1>
+            <p>Total simple Interest</p>
+         </div>
+         <form className='mt-5' onSubmit={handleCalculate}>
+             <div className='mb-3'>
+               <TextField name='principle' value={principle || ""} onChange={(e)=>getValidate(e)} className='w-100' id="outlined-basic" label="₹ Principle Amount" variant="outlined" /> 
+             </div>
+             {
+              !isprinciple &&
+              <div>
+                <p className='text-danger fw-bolder'>*Invalid Input</p>
+              </div>
+             }
+             <div className='mb-3'>
+               <TextField name='rate' value={rate || ""}  onChange={(e)=>getValidate(e)}   className='w-100' id="outlined-basic" label="Rate of Interest (p.a) %" variant="outlined" /> 
+             </div>
+             {
+              !israte &&
+              <div>
+                <p className='text-danger fw-bolder'>*Invalid Input</p>
+              </div>
+             }
+             <div className='mb-3'>
+               <TextField name='year' value={year || ""}  onChange={(e)=>getValidate(e)}  className='w-100' id="outlined-basic" label="Year (Yr)" variant="outlined" /> 
+             </div>
+             {
+              !isyear &&
+              <div>
+                 <p className='text-danger fw-bolder'>*Invalid Input</p>
+              </div>
+             }
+
+             <Stack direction="row" spacing={2} className='mt-5'>
+                <Button type='submit'  disabled={isprinciple && israte && isyear?false:true} className='bg-success' style={{width:'200px',height:'50px'}} variant="contained">Calculate</Button>
+
+                <Button onClick={handleReset} style={{width:'200px',height:'50px'}} variant="outlined">Reset</Button>
+             </Stack>
+         </form>
+      </div>
     </div>
   );
 }
